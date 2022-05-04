09b999d7ce2e6582e56cba218c73748e9773f141
NH Kariya <nh.kariya88@gmail.com>
2022-05-04 22:53:26 +0530

    - Deviation screen

---
+++ QBD2/Models/Deviation.cs
@@ -0,0 +1,16 @@
+﻿namespace QBD2.Models
+{
+    public class Deviation
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Item { get; set; }
+        public string Originator { get; set; }
+        public string ItemPortNumber { get; set; }
+        public string PortDescription { get; set; }
+        public string ReasonforManufacturingDeviation { get; set; }
+        public bool ECORequired { get; set; }
+        public string ECONumber { get; set; }
+        public string CommentCorrectiveAction { get; set; }
+    }
+}

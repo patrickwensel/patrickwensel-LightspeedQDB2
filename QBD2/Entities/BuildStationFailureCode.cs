a3b93dfefe43614dbf3bf307fbe861add4f57e44
Patrick Wensel <pwensel@hotmail.com>
2022-07-24 23:56:08 -0400

    Added BuildStationFailureCode

---
+++ QBD2/Entities/BuildStationFailureCode.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QBD2.Entities
+{
+    public class BuildStationFailureCode
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BuildStationFailureCodeId { get; set; }
+
+        public string Name { get; set; }
+
+        [ForeignKey("BuildStation")]
+        public int BuildStationId { get; set; }
+        public virtual BuildStation BuildStation { get; set; }
+    }
+}

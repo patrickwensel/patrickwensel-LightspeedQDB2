423434fb8b5f3d530ca8d6f0e771f9a1214c5014
Patrick Wensel <pwensel@hotmail.com>
2022-08-10 13:15:39 +0300

    MRB

---
+++ QBD2/Entities/MRBDisposition.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QBD2.Entities
+{
+    public class MRBDisposition
+    {
+        [Key]
+        public int MRBDispositionId { get; set; }
+
+        public string Name { get; set; }
+    }
+}

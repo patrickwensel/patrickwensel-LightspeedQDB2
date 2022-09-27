e32ffe32e670870ab712ba1a50a3b0f71cd3f593
Ravi <cptdev006@gmail.com>
2022-09-27 11:38:28 +0530

    Work on adding a serial number to the parts table (search Sage for the serial number)

---
+++ QBD2/Models/AddEditPartModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QBD2.Models
+{
+    public class AddEditPartModel
+    {
+        public int PartId { get; set; }
+
+        public int MasterPartId { get; set; }
+
+        [Required(ErrorMessage = "The Part Number field is required.")]
+        public string? MasterPartNumber { get; set; }
+
+        [Required(ErrorMessage = "The Serial Number field is required.")]
+        public string? SerialNumber { get; set; }
+    }
+}
